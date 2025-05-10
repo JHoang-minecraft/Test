5e37241e3d720dcb5d9cc34b3d231ba1b34e6311
Jhoang <hoangnenguyen907@gmail.com>
2025-05-10 11:05:07 +0700

    startup.lua

--- startup.lua
+++ startup.lua
@@ -1,2 +1,22 @@
-https://raw.githubusercontent.com/JHoang-minecraft/test/main/startup.lua
+local pastebinURL = "https://raw.githubusercontent.com/JHoang-minecraft/Test/main/startup.lua"
+local fileName = "startup.lua"
+
+print("Downloading file from " .. pastebinURL)
+local success, result = pcall(function() 
+    wget(pastebinURL, fileName)
+end)
+
+if success then
+    print("Download complete!")
+else
+    print("Error downloading file: " .. result)
+end
+
+if fs.exists(fileName) then
+    print("File successfully downloaded. Executing script...")
+    shell.run(fileName)  
+else
+    print("Failed to download the file.")
+end
+
 
